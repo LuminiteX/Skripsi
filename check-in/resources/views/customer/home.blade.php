cc80bb335fd412cbdf6f35f8230779c8a8737b26
JasonC019 <jcorneliuss47@gmail.com>
2023-04-14 14:00:36 +0700

    Update home our story and why choose us

--- check-in/resources/views/customer/home.blade.php
+++ check-in/resources/views/customer/home.blade.php
@@ -12,14 +12,10 @@
         </div>
         <div class="container bg-white text-body pt-5">
             <div class="row">
-                <div class="col mt-5">
-                    <h5 class="mt-5">OUR STORY</h5>
+                <div class="col mt-5 mb-5">
+                    <h5>OUR STORY</h5>
                     <h2>Welcome</h2>
-                    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Temporibus nemo incidunt praesentium, ipsum culpa
-                        minus eveniet, id nesciunt excepturi sit voluptate
-                        repudiandae. Explicabo, incidunt quia. Repellendus
-                        mollitia quaerat est voluptas!</h5>
+                    <h5>People around the world like to eat in restaurants, with most of them making reservations beforehand. We noticed that the reservation process and handling is a bit of a hassle for both restaurant owners as well as customers. With the COVID-19 pandemic happening recently, we have also seen the need for a way to reserve and order menu without physical interaction. Because of that, our team decided to make Check-In so that both restaurnat owners and customers would have an easier time in making and managing reservations.</h5>
                 </div>
                 <div class="col mb-5">
                     <img src="storage/HomeImg/Food.jpeg" alt="Food" class="img-fluid" style="max-width: 500px;">
@@ -31,11 +27,7 @@
                 <div class="col mt-5">
                     <h5 class="mt-5">About Us</h5>
                     <h1>WHY CHOOSE US?</h1>
-                    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Temporibus nemo incidunt praesentium, ipsum culpa
-                        minus eveniet, id nesciunt excepturi sit voluptate
-                        repudiandae. Explicabo, incidunt quia. Repellendus
-                        mollitia quaerat est voluptas!</h5>
+                    <h5>Our application has a lot of features to help you in making reservations online. Here's the three main reasons on why you should choose Check-In.</h5>
                     <div class="row mt-4">
                         <div class="col-sm-1 text-info">
                             <h5>1.</h5>
